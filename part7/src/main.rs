ed490bedafbe7ee954e250a7742eaf2691adbba0
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-08-14 10:33:58 -0700

    Completed the final exercise. Made it so the functions are borrowing references to the vec and setting up lifetime annotations for the slices that are being returned. That way they can be called without being deallocated

--- part7/src/main.rs
+++ part7/src/main.rs
@@ -1,20 +1,19 @@
 fn main() {
     let numbers = vec![2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24];
 
-    // 👉 TODO call sum(), product(), and average() to replace the `= 0` values
-    // here, without using .clone() or changing what those functions return.
-    //
-    // 💡 TIP: You can do this by accepting a slice type - e.g. &[i64]
-    let sum_of_nums = 0;
-    let product_of_nums = 0;
-    let average_of_nums = 0;
+    // Borrows the numbers vec for each of the functions
+    let sum_of_nums = sum(&numbers);
+    let product_of_nums = product(&numbers);
+    let average_of_nums = average(&numbers);
 
     println!("Sum of these numbers: {}", sum_of_nums);
     println!("Product of these numbers: {}", product_of_nums);
     println!("Average of these numbers: {}", average_of_nums);
 
     let other_numbers = vec![1, 2, 3, 4, 5, 6];
-    let (slice1, slice2) = first_three(numbers, other_numbers);
+    // Set slice 1 and slice 2 both equal to references to a vec
+    // Borrow the two different vecs(numbers1, numbers2) here inside the first_three function
+    let (slice1, slice2) = first_three(&numbers, &other_numbers);
 
     println!("The first three elements in `slice1` are:");
 
@@ -29,7 +28,8 @@ fn main() {
     }
 }
 
-fn sum(numbers: Vec<i64>) -> i64 {
+// Set numbers equal to a slice of the vec
+fn sum(numbers: &[i64]) -> i64 {
     let mut total = 0;
 
     for num in numbers.iter() {
@@ -39,7 +39,7 @@ fn sum(numbers: Vec<i64>) -> i64 {
     total
 }
 
-fn product(numbers: Vec<i64>) -> i64 {
+fn product(numbers: &[i64]) -> i64 {
     let mut total = 1;
 
     for num in numbers.iter() {
@@ -49,32 +49,16 @@ fn product(numbers: Vec<i64>) -> i64 {
     total
 }
 
-fn average(numbers: Vec<i64>) -> i64 {
+fn average(numbers: &[i64]) -> i64 {
     let length = numbers.len() as i64;
 
     sum(numbers) / length
 }
 
-fn first_three(numbers1: Vec<i64>, numbers2: Vec<i64>) -> (Vec<i64>, Vec<i64>) {
-    // 👉 TODO Return two slices, one containing the first 3 elements in
-    //    numbers1, and the other containing the first 3 elements in numbers2.
-    //
-    // 💡 TIP 1: You can get slices like this using this syntax:
-    //
-    //        &numbers[0..3]
-    //
-    // 💡 TIP 2: A slice type with a lifetime annotation looks like this:
-    //
-    //        &'a [i64]
-    //
-    // 💡 TIP 3: To introduce lifetime annotations to this function, change its
-    //    declaration to something like the following, depending on how many
-    //    different lifetimes you want to use:
-    //
-    //        fn first_three<'a>
-    //
-    //        fn first_three<'a, 'b>
-    //
-    //        fn first_three<'a, 'b, 'c>
-    (numbers1, numbers2)
+// This sets up the lifetime annotation. numbers1 has a lifetime annotation of a and numbers2 has a lifetime annotation of b
+// Then you call a reference to the vec and attach the annotations to each
+// Then you would get a return a slice of a vec with each separate lifetime annotation
+fn first_three<'a, 'b>(numbers1: &'a Vec<i64>, numbers2: &'b Vec<i64>) -> (&'a [i64], &'b [i64]) {
+    (&numbers1[0..3], &numbers2[0..3])
+    // This borrows from the numbers1 and numbers2 reference, and pulls the first three values from the vec
 }

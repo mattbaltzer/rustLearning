aedb3ec3e201b776638633acaec8a4fedb492391
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-08-14 08:37:26 -0700

    Updated the comments and changed the vecs to slices to properly test my knowledge of slices and borrowing

--- part6/src/main.rs
+++ part6/src/main.rs
@@ -8,18 +8,13 @@ fn main() {
     let product_of_nums = product(&numbers); 
     let average_of_nums = average(&numbers); 
 
-    // 💡 TIP: You'll get a compile error. Here are two ways you can fix it:
-    //
-    // Option 1: Accept &Vec<i64> instead of Vec<i64>
-    //
-    // Option 2: Accept a slice - that is, &[i64] - instead of a Vec
-    //
-    // Give both options a try!
-
     println!("Sum of these numbers: {}", sum_of_nums);
     println!("Product of these numbers: {}", product_of_nums);
     println!("Average of these numbers: {}", average_of_nums);
 }
+
+// If a fn accepts a slice, you can give it a borrowed vec &[i64]
+// This is because of traits, when you borrow a type, you can translate it into a different reference type
 
 fn sum(numbers: &[i64]) -> i64 {
     let mut total = 0;
